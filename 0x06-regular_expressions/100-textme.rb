083d0fd40bb04bbb811385828e4e38d2f41b5c91
Lamin Jawla <laminjawla1001@gmail.com>
2023-10-31 08:52:25 -0700

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,18 +1,2 @@
-#!/usr/bin/env python3
-import sys
-import re
-
-
-message = sys.argv[1]
-
-pattern = r'\[from:([^]]+)] \[to:([^]]+)] \[flags:([^]]+)]'
-
-match = re.search(pattern, message)
-
-if match:
-    _from = match.group(1)
-    _to = match.group(2);
-    _flags = match.group(3)
-    print(f"{_from},{_to},{_flags}")
-else:
-  print()
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
