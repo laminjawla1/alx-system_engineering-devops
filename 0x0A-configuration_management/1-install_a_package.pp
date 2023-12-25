1aba93d71775aedaed6a382b184cc045184f3cf2
Lamin Jawla <laminjawla1001@gmail.com>
2023-12-25 11:38:54 +0000

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
 # This script install flask from pip3
-exec {'pip3':
-  command  => 'pip3 install Flask==2.1.0',
-  provider => 'shell'
+exec {'Flask':
+  command  => '/usr/bin/pip3 install Flask==2.1.0',
 }
