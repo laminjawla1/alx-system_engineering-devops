61ba9f50e15047a8e4af4f3ea8ad852074888f8b
Lamin Jawla <laminjawla1001@gmail.com>
2023-12-25 11:34:34 +0000

    Client configuration file (w/ Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 # This file creates an ssh configuration file using puppet
-file {'ssh_config':
-  mode    => '0744',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  content => 'Host * PasswordAuthentication no IdentifyFile ~/.ssh/school'
+
+# No password authentcation
+file_line {'No Password Auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '	PasswordAuthentication no',
+  replace => true
+}
+
+# Set the default identity file
+file_line {'Default Identity File':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '	IdentityFile ~/.ssh/school',
+  replace => true
 }
