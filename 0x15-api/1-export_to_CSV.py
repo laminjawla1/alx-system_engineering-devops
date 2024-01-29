daea937a00c64d74d07b958316de323143719270
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-29 15:53:09 +0000

    Export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Write a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import csv
+import requests
+import sys
+
+
+def main():
+    url = "https://jsonplaceholder.typicode.com"
+    user = requests.get("{}/users/{}".format(url, sys.argv[1])).json()
+    todos = requests.get(
+            "{}/todos".format(url), params={"userId": sys.argv[1]}).json()
+
+    todos_completed = get_total_todos_completed(todos)
+    with open("{}.csv".format(user.get("id")), "w", newline="") as csvfile:
+        headers = [
+                "USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        writer = csv.writer(csvfile)
+
+        writer.writerow(headers)
+        for todo in todos:
+            writer.writerow(
+                [
+                    user.get("id"),
+                    user.get("username"),
+                    todo.get("completed"),
+                    todo.get("title"),
+                ]
+            )
+
+
+def get_total_todos_completed(todos):
+    total = 0
+    for todo in todos:
+        if todo.get("completed"):
+            total += 1
+    return total
+
+
+if __name__ == "__main__":
+    main()
