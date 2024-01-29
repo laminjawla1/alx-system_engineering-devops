6f02a7b7a0bdfb6a3272a2bf5bfd6a7f1ba57383
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-29 15:15:51 +0000

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,8 +3,8 @@
 Write a Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
-import sys
 import requests
+import sys
 
 
 def main():
