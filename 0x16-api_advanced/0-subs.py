e54da0a33e55d286e455e2128eae5e5ea717d697
Lamin Jawla <laminjawla1001@gmail.com>
2024-02-13 07:46:47 -0500

    Task 0 done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-A script to get the total subscribers of a subreddit
+A script to get the total subscribers
+of a specified subreddit
 """
 import requests
 import sys
@@ -12,5 +13,6 @@ def number_of_subscribers(subreddit):
         headers={"User-Agent": "I-Am-Agent-Sage"},
         allow_redirects=False,
     )
-    data = response.json().get("data") if response.status_code < 300 else None
-    return data.get("subscribers", 0) if data else 0
+    if response.status_code >= 300:
+        return 0
+    return response.json().get("data").get("subscribers", 0)
