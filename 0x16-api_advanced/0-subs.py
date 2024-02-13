0b07fd8dc05e5796c22b97b066ebe4d2e7da2bdf
Lamin Jawla <laminjawla1001@gmail.com>
2024-02-13 08:09:54 -0500

    0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    Return the number of subscribers
+    """
     response = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "I-Am-Agent-Sage"},
+        headers={"User-Agent": "My-User-Agent"},
         allow_redirects=False,
     )
     if response.status_code >= 300:
         return 0
-    return response.json().get("data").get("subscribers", 0)
+    return response.json().get("data").get("subscribers")
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -1,2 +1,3 @@
 # 0x16. API advanced
 
+API stands for Application Programming Interface. In the context of APIs, the word Application refers to any software with a distinct function. Interface can be thought of as a contract of service between two applications. This contract defines how the two communicate with each other using requests and responses.
